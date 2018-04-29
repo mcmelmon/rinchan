6eed2219dc516d052e46fe8d8c51f072369744a2
Matthew Melmon <matt.melmon@gmail.com>
2018-04-29 15:24:37 -0700

    Edit and delete posts

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -1,8 +1,11 @@
 class BulletinsController < ApplicationController
+  before_action :logged_in_user
+  before_action :correct_user
+
   def create
     @bulletin = current_user.bulletins.build(bulletin_params)
     if @bulletin.save
-      flash[:success] = "Post created!"
+      flash[:notice] = "Post created!"
       redirect_to user_path(current_user)
     else
       redirect_to root_path
@@ -10,17 +13,40 @@ class BulletinsController < ApplicationController
   end
 
   def destroy
+    @bulletin.destroy
+    flash[:notice] = "Post deleted."
+    redirect_to request.referrer || root_url
   end
 
   def edit
   end
 
   def update
+    if @bulletin.update(bulletin_params)
+      flash[:notice] = "Post updated."
+      redirect_to user_path(current_user)
+    else
+      flash[:error] = "There was a problem."
+      redirect_to user_path(current_user)
+    end
   end
 
   private
 
     def bulletin_params
       params.require(:bulletin).permit(:body, :title)
+    end
+
+    def correct_user
+      @bulletin = current_user.bulletins.find_by(id: params[:id])
+      redirect_to root_url if @bulletin.nil?
+    end
+
+    def logged_in_user
+      #TODO: make into a concern
+      unless user_signed_in?
+        flash[:danger] = "Please log in."
+        redirect_to user_session_path
+      end
     end
 end
