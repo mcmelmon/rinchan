d7e2746c0b81f3da38eb1abfb6129c64fb450d1c
Matthew Melmon <matt.melmon@gmail.com>
2018-05-03 11:06:08 -0700

    Use japanese as default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  require 'will_paginate/array'
+  before_action :set_locale
+
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
+
+  private
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
 end
