f3c8ed7c56ccc509410f043207ce674c6f6be36b
Matthew Melmon <matt.melmon@gmail.com>
2018-05-03 14:52:56 -0700

    Add topic heading/tabs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    if params[:sort] == 'replies'
+    if params[:sort].present?
       @topics = Topic.by('replies').paginate(page: params[:page])
     else
       @topics = Topic.all.paginate(page: params[:page])
