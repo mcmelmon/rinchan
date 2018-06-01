81be3f179bb742eeb0e00bfa8e463a6ccb85275e
Matthew Melmon <matt.melmon@gmail.com>
2018-05-31 22:19:04 -0700

    Delete keys

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => "AKIAI543OEVTQNLAWDBA",
-      :aws_secret_access_key  => "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",
+      :aws_access_key_id      => "",
+      :aws_secret_access_key  => "",
       :region                 => 'us-west-1' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = "rinchan-assets"
