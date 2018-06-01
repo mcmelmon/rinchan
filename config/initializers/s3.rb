d9e1fcd8cba9205bde0097f7c6c9611ba368dbfd
Matthew Melmon <matt.melmon@gmail.com>
2018-05-31 22:33:25 -0700

    Use figaro

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => "",
-      :aws_secret_access_key  => "",
+      :aws_access_key_id      => ENV['AWS_KEY'],
+      :aws_secret_access_key  => ENV['AWS_SECRET'],
       :region                 => 'us-west-1' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = "rinchan-assets"
